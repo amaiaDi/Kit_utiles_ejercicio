7e41224e75e5b56ba8cbc66f9dab76c5620da655
amaiaDi <amaia.di@icjardin.com>
2025-10-27 18:50:19 +0100

    modificacion de test

--- tests/test_cov02_cli_cases.py
+++ tests/test_cov02_cli_cases.py
@@ -1,6 +1,7 @@
 from kit_utiles.cli import main, _format_number
 import sys
 
+#TODO: añadir más tests de CLI si es necesario
 def test_cli_sin_argumentos(capsys):
     main(["prog"])
     assert capsys.readouterr().out.strip() == "0"
